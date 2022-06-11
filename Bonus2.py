67f6c928fd7430acb0b898f3070fe703d97e4379
Christian van den Berg <christian.vandenberg@hotmail.nl>
2022-06-11 20:06:25 +0200

    Saved data from exponent 5 to 13 into csv

--- Bonus2.py
+++ Bonus2.py
@@ -1,9 +1,11 @@
-import imp
+
 import networkx as nx
 import matplotlib.pyplot as plt
 import time
 import numpy as np
 import imp
+
+
 
 class Graph:
     
@@ -11,7 +13,7 @@ class Graph:
         self.verticesnames=[]
         self.vertices=0
         self.graph = []
-        
+    
     def addEdge(self, u, v, w: float)->None: #  u to  v are vertices and float w is the directional weight
         if u not in self.verticesnames:
             self.verticesnames.append(u)
@@ -20,7 +22,8 @@ class Graph:
             self.verticesnames.append(v)
             self.vertices=len(self.verticesnames)
         self.graph.append([self.verticesnames.index(u),self.verticesnames.index(v),w])
-        
+    
+    #@njit
     def BellmanFord(self, startvertices, endvertices)->None:
         distvec= [float("Inf")]*self.vertices 
         distvec[self.verticesnames.index(startvertices)]=0 
@@ -31,33 +34,27 @@ class Graph:
                     distvec[v] = distvec[u] + w
                     
         self.printSolution(distvec,startvertices,endvertices)
-        
+    
+
     def printSolution(self, distvec,startvertices,endvertices)->None:
         for i in range(self.vertices):
             if self.verticesnames.index(endvertices)==i:
                 print(f"shortest path from {startvertices} to {endvertices} is {distvec[i]}")
-                
+    
+
     def addMatrix(self,matrix):
         for u,v,w in matrix:
             self.addEdge(u,v,w)
-    
-                
-if __name__ == "__main__":
+            
 
-    # matrix=[['A', 'B', 1], # form A to B with weight 1
-    # ['B', 'C', 4],
-    # ['C','D', 4],
-    # ['A','C',7],
-    # ['A','E',12],
-    # ['D','E',1]]
-    
-    nums=range(5,9)
+def analysis(n):
+    nums=range(5,n)
     datamat=[]
     for num in nums:
         timedata=[]
         print(f"next num {num}")
         for dum in range(20):
-            tic = time.perf_counter()
+            
             N=2**num
             p=20/(N-1)
         
@@ -74,11 +71,26 @@ if __name__ == "__main__":
 
             graph = Graph()
             graph.addMatrix(matrix2)
+            tic = time.perf_counter()
             graph.BellmanFord(1,(2**num)-1)
             toc = time.perf_counter()
             timedata.append(toc-tic)
         datamat.append(timedata)
-        
+    return datamat, nums
+    
+                
+if __name__ == "__main__":
+
+    # matrix=[['A', 'B', 1], # form A to B with weight 1
+    # ['B', 'C', 4],
+    # ['C','D', 4],
+    # ['A','C',7],
+    # ['A','E',12],
+    # ['D','E',1]]
+    
+
+    datamat, nums = analysis(14)
+    np.savetxt('datamat.csv', datamat, delimiter=',') 
     print(datamat)
     datamatnp=np.array(datamat)
     meantime=np.mean(datamatnp,axis=1)
